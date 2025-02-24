04c2afa132b06338e13d51d24084e1d8d9c6d11d
AustinSchlutter <90343739+austinjames95@users.noreply.github.com>
2025-02-24 12:49:41 -0500

    Update halfAdd.vhd

--- project5_w_half_add/halfAdd.vhd
+++ project5_w_half_add/halfAdd.vhd
@@ -3,17 +3,17 @@ USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY half_adder IS
     PORT (
-        A, B  : IN  STD_LOGIC;
-        S     : OUT STD_LOGIC;
-        Cout  : OUT STD_LOGIC
-    );
+        A : IN  STD_LOGIC;
+        B : IN STD_LOGIC;
+        S : OUT STD_LOGIC;
+        Cout : OUT STD_LOGIC);
 END half_adder;
 
 ARCHITECTURE Behavioral OF half_adder IS
 BEGIN
     -- Sum calculation (XOR)
-    S    <= A XOR B;
+    S <= A XOR B;
 
     -- Carry calculation (AND)
     Cout <= A AND B;
-END Behavioral;
+END ARCHITECTURE Behavioral;
