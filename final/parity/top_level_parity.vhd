e68b43966a64f4bfb0d727d76c7a5f3f46235a63
AustinSchlutter <90343739+austinjames95@users.noreply.github.com>
2025-03-26 12:41:54 -0400

    Update top_level_parity.vhd

--- final/parity/top_level_parity.vhd
+++ final/parity/top_level_parity.vhd
@@ -1,42 +1,33 @@
+Top_level_parity:
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity top_level_parity is
     Port ( 
-        sw, sw2 : in STD_LOGIC_VECTOR (3 downto 0);
-        led_parity : out STD_LOGIC;
-        seg : out STD_LOGIC_VECTOR (6 downto 0);
-        an  : out STD_LOGIC_VECTOR (3 downto 0)
+        sw, sw2     : in  STD_LOGIC_VECTOR (3 downto 0);
+        sum_out     : out STD_LOGIC_VECTOR (3 downto 0);
+        led_parity  : buffer STD_LOGIC
     );
-end top_level;
+end top_level_parity;
 
 architecture Behavioral of top_level_parity is
-    signal mid : STD_LOGIC_VECTOR(3 downto 0);
+    signal ones_counter : integer <= 0;
+    signal full_sum : STD_LOGIC_VECTOR(4 downto 0);
 begin
+    ones_count <= to_integer(unsigned(sw(0))) +
+                  to_integer(unsigned(sw(1))) +
+                  to_integer(unsigned(sw(2))) +
+                  to_integer(unsigned(sw(3))) +
+                  to_integer(unsigned(sw2(0))) +
+                  to_integer(unsigned(sw2(1))) +
+                  to_integer(unsigned(sw2(2))) +
+                  to_integer(unsigned(sw2(3)));
 
-    led_parity <= '1' when sw(0) = sw2(0) else '0';
-
-    -- Enable only one display (e.g., the rightmost digit)
-    an <= "1110";
-    mid <= sw;
-
-    with mid select
-    seg <=
-        "1000000" when "0000", 
-        "1111001" when "0001", 
-        "0100100" when "0010", 
-        "0110000" when "0011", 
-        "0011001" when "0100", 
-        "0010010" when "0101", 
-        "0000010" when "0110", 
-        "1111000" when "0111", 
-        "0000000" when "1000", 
-        "0010000" when "1001", 
-        "0001000" when "1010", 
-        "0000011" when "1011", 
-        "1000110" when "1100", 
-        "0100001" when "1101", 
-        "0000110" when "1110", 
-        "0001110" when others; 
+    full_sum <= std_logic_vector(to_unsigned(ones_count, 5));
+    sum_out <= full_sum(3 downto 0);
+    led_parity <= sw(0) xor sw(1) xor sw(2) xor sw(3) xor sw2(0) xor sw2(1) xor sw2(2) xor sw2(3);
 
 end Behavioral;
+
